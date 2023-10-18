dd215815d888ead9d196acf83d7a978fec8b73b6
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2023-10-17 21:41:20 -0300

    Front melhorado

--- src/main/resources/public/js/login.js
+++ src/main/resources/public/js/login.js
@@ -1,19 +1,16 @@
-document.querySelector("#form").addEventListener("submit", function (event) {
-  event.preventDefault(); // Impede o envio padrão do formulário
 
-  let user = document.querySelector("#nome").value;
-  let senha = document.querySelector("#senha").value;
-
-  let url = `/user?nome=${encodeURIComponent(user)}&senha=${encodeURIComponent(senha)}`;
-
-  fetch(url)
-    .then(function (response) {
-      return response.text();
+let user = document.querySelector("#nome").value;
+let senha = document.querySelector("#senha").value;
+document.querySelector("#nome").addEventListener("submit", () => {
+    let url = `/user?nome=${encodeURIComponent(user)}&senha=${encodeURIComponent(senha)}`
+    fetch(url)
+    .then(function(response) {
+        return response.text();
+        })
+    .then(function(data) {
+        console.log(data);
     })
-    .then(function (data) {
-      console.log(data);
-    })
-    .catch(function (error) {
-      console.error("Erro ao fazer login:", error);
+    .catch(function(error) {
+        console.error("Erro ao fazer login:", error);
     });
 });
